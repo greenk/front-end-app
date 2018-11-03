542d8a9ea49007c47633be0b0b459db3a311c60c
tannv2 <mralone1204@gmail.com>
2018-11-03 23:33:39 +0700

    Add auto submit when input 4 characters

--- assets/tannv/js/scan-ticket.js
+++ assets/tannv/js/scan-ticket.js
@@ -5,6 +5,24 @@
     $(document).ready(function() {
         Site.run();
 
-        
+        //// Input event (Support full browser, include IE version < 9)
+        $('#scan-input').on('propertychange input', function (e) {
+
+          var valueChanged = false;
+          if (e.type=='propertychange') {
+              valueChanged = e.originalEvent.propertyName=='value';
+          } else {
+              valueChanged = true;
+          }
+          if (valueChanged) {
+
+            // If value is changed
+            var content = $(this).val().trim();
+            if(content !== '' && content.length == 4) {
+              console.log("submit");
+
+            }
+          }
+        });
     });
 })(document, window, jQuery);
