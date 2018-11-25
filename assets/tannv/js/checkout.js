bca97d968699fd12201cab93fe00945eae57656e
tannv2 <mralone1204@gmail.com>
2018-11-25 23:56:40 +0700

    temp

---
+++ assets/tannv/js/checkout.js
@@ -0,0 +1,23 @@
+(function(document, window, $) {
+    'use strict';
+    var Site = window.Site;
+    var _jquery2 = babelHelpers.interopRequireDefault($);
+    $(document).ready(function() {
+        Site.run();
+
+        // Popup quicktip
+        // Find info in https://github.com/sandywalker/webui-popover
+        $('#btn-quick-tip').webuiPopover(tableSettings);
+        $('#btn-quick-tip2').webuiPopover(tableSettings);
+    });
+})(document, window, jQuery);
+
+// Option Quick Tip
+var tableContent = $('#quicktip-popover').html(),
+    tableSettings = {
+      title: '',
+      content: tableContent,
+      width: 282,
+      animation: 'fade',
+      placement: 'bottom'
+    };
