03a5c1bde40932044b4927d1d2c32a101dbd288c
anhtt <gacongngheee@gmail.com>
2019-03-06 21:52:08 +0700

    #appointmentEmployee(tablet) #viewByW01 #doing

--- assets/anhtt/js/appointment_employer.js
+++ assets/anhtt/js/appointment_employer.js
@@ -37,7 +37,8 @@
                 prev.after(elementToAdd);
             });
         });
-        addDetailElement();
+        rendCalendar();
+        // addDetailElement();
     });
 })(document, window, jQuery);
 
@@ -114,4 +115,205 @@ function addAppointmentElement(timeDoing) {
         "                                            </div>\n" +
         "                                        </div>\n" +
         "                                    </div>";
+}
+
+// tui-calendar: https://github.com/nhnent/tui.calendar/blob/master/docs/getting-started.md
+function rendCalendar() {
+    var themeConfig = {
+        'common.border': '1px solid #e5e5e5',
+        'common.backgroundColor': 'white',
+        'common.holiday.color': '#263238',
+        'common.saturday.color': '#263238',
+        'common.dayname.color': '#263238',
+        'common.today.color': '#263238',
+
+        // creation guide style
+        'common.creationGuide.backgroundColor': 'rgba(81, 92, 230, 0.05)',
+        'common.creationGuide.border': '1px solid #515ce6',
+
+        // month header 'dayname'
+        'month.dayname.height': '31px',
+        'month.dayname.borderLeft': '1px solid #e5e5e5',
+        'month.dayname.paddingLeft': '10px',
+        'month.dayname.paddingRight': '10px',
+        'month.dayname.backgroundColor': 'inherit',
+        'month.dayname.fontSize': '12px',
+        'month.dayname.fontWeight': 'normal',
+        'month.dayname.textAlign': 'left',
+
+        // month day grid cell 'day'
+        'month.holidayExceptThisMonth.color': 'rgba(255, 64, 64, 0.4)',
+        'month.dayExceptThisMonth.color': 'rgba(51, 51, 51, 0.4)',
+        'month.weekend.backgroundColor': 'inherit',
+        'month.day.fontSize': '14px',
+
+        // month schedule style
+        'month.schedule.borderRadius': '2px',
+        'month.schedule.height': '24px',
+        'month.schedule.marginTop': '2px',
+        'month.schedule.marginLeft': '8px',
+        'month.schedule.marginRight': '8px',
+
+        // month more view
+        'month.moreView.border': '1px solid #d5d5d5',
+        'month.moreView.boxShadow': '0 2px 6px 0 rgba(0, 0, 0, 0.1)',
+        'month.moreView.backgroundColor': 'white',
+        'month.moreView.paddingBottom': '17px',
+        'month.moreViewTitle.height': '44px',
+        'month.moreViewTitle.marginBottom': '12px',
+        'month.moreViewTitle.backgroundColor': 'white',
+        'month.moreViewTitle.borderBottom': 'none',
+        'month.moreViewTitle.padding': '12px 17px 0 17px',
+        'month.moreViewList.padding': '0 17px 17px 17px',
+
+        // week header 'dayname'
+        'week.dayname.height': '44px',
+        'week.dayname.borderTop': 'none',
+        'week.dayname.borderBottom': '1px solid #e5e5e5',
+        'week.dayname.borderLeft': 'inherit',
+        'week.dayname.date.color': 'red',
+        'week.dayname.paddingLeft': '0',
+        'week.dayname.backgroundColor': 'inherit',
+        'week.dayname.textAlign': 'center',
+        'week.dayname.float': 'bottom',
+        'week.today.color': '#263238',
+
+        // week vertical panel 'vpanel'
+        'week.vpanelSplitter.border': '1px solid #e5e5e5',
+        'week.vpanelSplitter.height': '3px',
+
+        // week daygrid 'daygrid'
+        'week.daygrid.borderRight': '1px solid #e5e5e5',
+        'week.daygrid.backgroundColor': 'inherit',
+
+        'week.daygridLeft.width': '72px',
+        'week.daygridLeft.backgroundColor': 'inherit',
+        'week.daygridLeft.paddingRight': '8px',
+        'week.daygridLeft.borderRight': '1px solid #e5e5e5',
+
+        'week.today.backgroundColor': 'rgba(81, 92, 230, 0.05)',
+        'week.weekend.backgroundColor': 'inherit',
+
+        // week timegrid 'timegrid'
+        'week.timegridLeft.width': '72px',
+        'week.timegridLeft.backgroundColor': 'inherit',
+        'week.timegridLeft.borderRight': '1px solid #e5e5e5',
+        'week.timegridLeft.fontSize': '11px',
+        'week.timegridLeftTimezoneLabel.height': '20px',
+
+        'week.timegridOneHour.height': '52px',
+        'week.timegridHalfHour.height': '26px',
+        'week.timegridHalfHour.borderBottom': 'none',
+        'week.timegridHorizontalLine.borderBottom': '1px solid #e5e5e5',
+
+        'week.timegrid.paddingRight': '8px',
+        'week.timegrid.borderRight': '1px solid #e5e5e5',
+        'week.timegridSchedule.borderRadius': '2px',
+        'week.timegridSchedule.paddingLeft': '2px',
+
+        'week.currentTime.color': '#515ce6',
+        'week.currentTime.fontSize': '11px',
+        'week.currentTime.fontWeight': 'normal',
+
+        'week.pastTime.color': '#333',
+        'week.pastTime.fontWeight': 'normal',
+
+        'week.futureTime.color': '#333',
+        'week.futureTime.fontWeight': 'normal',
+
+        'week.currentTimeLinePast.border': '1px dashed #515ce6',
+        'week.currentTimeLineBullet.backgroundColor': '#515ce6',
+        'week.currentTimeLineToday.border': '1px solid #515ce6',
+        'week.currentTimeLineFuture.border': 'none',
+
+        // week creation guide style
+        'week.creationGuide.color': '#515ce6',
+        'week.creationGuide.fontSize': '11px',
+        'week.creationGuide.fontWeight': 'bold',
+
+        // week daygrid schedule style
+        'week.dayGridSchedule.borderRadius': '2px',
+        'week.dayGridSchedule.height': '24px',
+        'week.dayGridSchedule.marginTop': '2px',
+        'week.dayGridSchedule.marginLeft': '8px',
+        'week.dayGridSchedule.marginRight': '8px'
+    };
+
+    var Calendar = tui.Calendar;
+    var calendar = new Calendar('#calendar', {
+        defaultView: 'week',
+        taskView: false,
+        scheduleView: ['time'],
+        isAllDay: false,
+        week: {
+            startDayOfWeek: 1,
+            hourStart: 7
+        },
+        theme: themeConfig,
+        template: {
+            weekDayname: function(model) {
+                var splitDatetime = model.renderDate.split("-");
+                var getMonthName = convertMonthNumberToMonthName(splitDatetime[1]);
+                return '<span style="    font-size: 16px !important;\n' +
+                    '    font-weight: 500;\n' +
+                    '    line-height: 1.25;\n' +
+                    '    letter-spacing: normal;\n' +
+                    '    text-align: left;\n' +
+                    '    padding-left: 8px; \n' +
+                    '    color: #263238;' +
+                    '    display: block">' +
+                    '' +  model.dayName +
+                    '</span><span style="    font-size: 13px;\n' +
+                    '    font-weight: normal;\n' +
+                    '    font-style: normal;\n' +
+                    '    display: block;\n' +
+                    '    text-align: left; \n' +
+                    '    padding-left: 8px; \n' +
+                    '    font-stretch: normal;\n' +
+                    '    line-height: 1.54;\n' +
+                    '    letter-spacing: normal;\n' +
+                    '    color: #76838f;" >' + splitDatetime[2] + ' ' + getMonthName + '</span>';
+            },
+        }
+    });
+
+    calendar.createSchedules([
+        {
+            id: '1',
+            calendarId: '1',
+            title: 'my schedule',
+            category: 'time',
+            dueDateClass: '',
+            start: '2019-03-06T22:30:00+09:00',
+            end: '2019-03-06T02:30:00+09:00'
+        },
+        {
+            id: '2',
+            calendarId: '1',
+            title: 'second schedule',
+            category: 'time',
+            dueDateClass: '',
+            start: '2019-03-05T17:30:00+09:00',
+            end: '2019-03-05T17:31:00+09:00',
+            isReadOnly: true    // schedule is read-only
+        }
+    ]);
+
+    // default keys and styles
+}
+function convertMonthNumberToMonthName(monthNumber) {
+    var months = {
+        "01" : "Jan",
+        "02" : "Feb",
+        "03" : "Mar",
+        "04" : "Apr",
+        "05" : "May",
+        "06" : "Jun",
+        "07" : "Jul",
+        "08" : "Aug",
+        "09" : "Sep",
+        "10" : "Oct",
+        "11" : "Nov",
+        "12" : "Dec"};
+    return months[monthNumber];
 }
