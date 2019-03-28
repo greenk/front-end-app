e53205de4412a40c57b842f4cca657f6b2b53655
anhtt <gacongngheee@gmail.com>
2019-03-28 16:38:43 +0700

    customer #done

--- assets/anhtt/js/customer.js
+++ assets/anhtt/js/customer.js
@@ -4,11 +4,14 @@
     var _jquery2 = babelHelpers.interopRequireDefault($);
     var modalCreateGift = $('#modal-create-gift-card');
     $(document).ready(function() {
-        document.getElementById('change-theme-btn').onclick = function() {
-            var themeHref = document.getElementById('customer-change-theme').href;
-            var explode = themeHref.split('/');
-            explode[explode.length - 1] = explode[explode.length - 1] === 'customer-dark.css' ? 'customer-light.css' : 'customer-dark.css';
-            document.getElementById('customer-change-theme').href = explode.join('/');
-        };
+        // document.getElementById('change-theme-btn').onclick = function() {
+        //     var themeHref = document.getElementById('customer-change-theme').href;
+        //     var explode = themeHref.split('/');
+        //     explode[explode.length - 1] = explode[explode.length - 1] === 'customer-dark.css' ? 'customer-light.css' : 'customer-dark.css';
+        //     document.getElementById('customer-change-theme').href = explode.join('/');
+        // };
+        $('#request-nail-tech').on('click', function () {
+           $('#mdl-customer-request-nails').modal('show');
+        });
     });
 })(document, window, jQuery);
